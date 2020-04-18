65277de66808081efc805b7084098d73a129d609
Minhhoang <hoangvanminh007>
2020-04-18 22:36:00 +0700

    chang files after compressed

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 let slideIndex = 0;
 showSlides();
+showSlidesRight()
 
 function showSlides() {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
+  let slides = document.querySelectorAll("#slide01");
+  // console.log(slides)
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -11,4 +13,17 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 3 seconds
+}
+
+function showSlidesRight() {
+  let i;
+  let slides = document.querySelectorAll("#slide02");
+  // console.log(slides)
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlidesRight, 3000); // Change image every 3 seconds
 }
